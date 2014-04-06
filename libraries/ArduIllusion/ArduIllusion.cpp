bd352f20d5c9f65d9d409c04c9bca7d681728e16
Stefan Gofferje <stefan@gofferje.net>
2014-04-06 17:07:02 +0300

    061406ZApr14

---
+++ libraries/ArduIllusion/ArduIllusion.cpp
@@ -0,0 +1,87 @@
+//
+// Library for controlling FLight Illusion sim gauges
+//
+// Copyright (c) 2013 Stefan Gofferje. All rights reserved.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later
+// version.
+//
+// This library is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General
+// Public License along with this library; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+// Boston, MA 02110-1301 USA
+//
+
+#include <FastSerial.h>
+#include <ArduIllusion.h>
+
+// ----- Constructor ---------------------------------------------------------
+FIGaugeSet::FIGaugeSet() {
+  
+}
+// ----- Private -------------------------------------------------------------
+void FIGaugeSet::sendCommand(char id, char cmd, long value) {
+  char buffer[]={0x00,0x00,0x00,0x00,0x00,0x00};
+  char bytethree, int2, cnt;
+  long long1;
+  buffer[1]=id;      // Gauge ID
+  buffer[5]=0xff;    // Terminator
+  
+  long1 = abs(value);
+  int2 = long1 >> 8;
+  
+  buffer[3]=(long1 & 0xff) | 0x01;
+  buffer[4]=(int2 & 0xff) | 0x02;
+
+  bytethree = cmd << 4;
+  bytethree |= (long1 & 0x01);
+  bytethree |= (int2 & 0x02);
+  if (value < 0) bytethree |= 0x0C;
+  else bytethree |= 0x08; 
+
+  buffer[2]=bytethree & 0xff;
+  
+  for (cnt=0;cnt<=5;cnt++) {
+    gaugePort.write(buffer[cnt]);
+  }
+}
+
+// ----- Public --------------------------------------------------------------
+
+void FIGaugeSet::Init() {
+  sendCommand(103,8,0);
+  delay(100);
+  sendCommand(103,3,64);
+  delay(100);
+  sendCommand(103,3,-64);
+  delay(100);
+  sendCommand(103,1,0);
+  delay(4000);
+  sendCommand(103,8,192);
+  delay(100);
+}
+
+void FIGaugeSet::setLight(char light) {
+  sendCommand(103,8,light);
+}
+
+void FIGaugeSet::setRoll(float angle) {
+  sendCommand(103,4,(360-(angle+rollOffset))*rollSteps);
+}
+
+void FIGaugeSet::setPitch(float angle) {
+  sendCommand(103,5,-1*angle*pitchSteps+pitchOffset);
+}
+  
+
+  
+  
---
+++ libraries/ArduIllusion/ArduIllusion.h
@@ -0,0 +1,53 @@
+//
+// Library for controlling FLight Illusion sim gauges
+//
+// Copyright (c) 2013 Stefan Gofferje. All rights reserved.
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later
+// version.
+//
+// This library is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+// PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General
+// Public License along with this library; if not, write to the
+// Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+// Boston, MA 02110-1301 USA
+//
+
+#ifndef ArduIllusion_h
+#define ArduIllusion_h
+
+#if ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#ifndef gaugePort
+#define gaugePort Serial3
+#endif
+
+#define rollSteps 10.667
+#define rollOffset 180
+#define pitchSteps 24
+#define pitchOffset 600
+
+
+class FIGaugeSet {
+  public:
+    FIGaugeSet();
+    void Init();
+    void setLight(char light);
+    void setRoll(float angle);
+    void setPitch(float angle);
+  private:
+    void sendCommand(char id, char cmd, long value);
+};
+#endif
